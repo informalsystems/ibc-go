654da43e170a06b14212e7e51b859f0dacef1cc1
Simon Noetzlin <simon.ntz@gmail.com>
2022-10-20 18:07:28 +0200

    save debug changes

--- proto/scripts/protocgen.sh
+++ proto/scripts/protocgen.sh
@@ -16,16 +16,17 @@ set -eo pipefail
 
 echo "Generating gogo proto code"
 cd proto
-# proto_dirs=$(find ./ibc -path -prune -o -name '*.proto' -print0 | xargs -0 -n1 dirname | sort | uniq)
-# for dir in $proto_dirs; do
-#   for file in $(find "${dir}" -maxdepth 2 -name '*.proto'); do
-buf generate --template buf.gen.gogo.yaml
-#   done
-# done
+proto_dirs=$(find ./ibc -path -prune -o -name '*.proto' -print0 | xargs -0 -n1 dirname | sort | uniq)
+for dir in $proto_dirs; do
+  for file in $(find "${dir}" -maxdepth 2 -name '*.proto'); do
+    buf --debug generate --template buf.gen.gogo.yaml
+  done
+done
 
 cd ..
 
+go mod tidy
 
 # move proto files to the right places
-cp -r github.com/cosmos/ibc-go/* ./
+cp -r github.com/cosmos/ibc-go/v5/* ./
 rm -rf github.com
